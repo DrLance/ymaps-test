b34cbabee97d4c2dc2a8c2c3ebffdb8a034c0da4
Ruslan <lyapusov@mariinsky.ru>
2018-02-05 18:42:55 +0300

    Add search on yandex map

--- src/theme/page-order_car.php
+++ src/theme/page-order_car.php
@@ -1,7 +1,21 @@
 <?php get_header(); ?>
     <!-- container -->
     <div class="container">
-        <div id="map" style="width: 1200px; height: 1000px"></div>
-    </div>
+        <table align="center" style="width:50%">
+					<tr>
+					  <td>Пункт А:<div id="pointA"></div></td>
+            <td>Пункт Б:<div id="pointB"></div></td>
+          </tr>
+          <tr>
+            <td>Координаты:<div id="map_coordsA"></div></td>
+            <td>Координаты<div id="map_coordsB"></div></td>
+          </tr>
+         <tr>
+          <td colspan="2">Расстояние:<div id="Route_length"></div></td>
+        </tr>
+      </table>
+      <div id="map" style="width: 1200px; height: 1000px"></div>
+  </div>
+
     <!-- /container -->
 <?php get_footer(); ?>
